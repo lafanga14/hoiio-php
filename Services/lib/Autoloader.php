0529c1fabd15adca00fd1619a4e4052d5b0ce6b0
lafanga14 <teju_baba@hotmail.com>
2014-07-07 11:43:36 +0800

    Autoloader.php

---
+++ Services/lib/Autoloader.php
@@ -0,0 +1,40 @@
+<?php
+
+HoiioService_Autoloader::Register();
+
+class HoiioService_Autoloader
+{
+    public static function Register() {
+        if (function_exists('__autoload')) {
+            spl_autoload_register('__autoload');
+        }
+        return spl_autoload_register(array('HoiioService_Autoloader', 'Load'));
+    }   //    function Register()
+
+
+    /**
+     * Autoload a class identified by name
+     *
+     * @param    string    $pClassName        Name of the object to load
+     */
+    public static function Load($pClassName){
+        if ((class_exists($pClassName,FALSE))) {
+            //already loaded 
+            return FALSE;
+        }
+
+        $pClassFilePath = HoiioService_ROOT .
+                          str_replace('_',DIRECTORY_SEPARATOR,$pClassName) .
+                          '.php';
+        if ((file_exists($pClassFilePath) === FALSE) || (is_readable($pClassFilePath) === FALSE)) {
+            //    Can't load
+            return FALSE;
+        }
+
+        require($pClassFilePath);
+    }   //    function Load()
+
+}
+
+
+?>
